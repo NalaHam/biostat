3fc0bc1c382f261a93cd2e028a5b8f8d22272f22
NalaHam <85026582+NalaHam@users.noreply.github.com>
2023-10-11 16:39:53 -0400

    Create assingment_5.R

---
+++ assingment_5.R
@@ -0,0 +1,7 @@
+#assignment associated with lab 7
+
+#Use the “Mouse_DNA_methylation_Aging.csv” to generate bar charts for all the data, the x-
+#axis is the Age_Months with a name of “Age of the mice”, the y-axis is the mean with a name of
+#"Average DNA Methylation". The title of the chart is "DNA methylation level in GF and SPF
+#mice at different ages". The error bars are for the mean=+/-SEM. The background is clean
+#without gray color or any grids. Show me your R command lines and the final graph.
