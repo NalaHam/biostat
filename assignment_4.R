9e01359a56fbdd5437f2bb982953928cbf1370a5
NalaHam <85026582+NalaHam@users.noreply.github.com>
2023-10-26 15:46:46 -0400

    Update assignment_4.R

--- assignment_4.R
+++ assignment_4.R
@@ -1,5 +1,6 @@
 #assignment for lab 6
 #Do NOT use the function “rank()” or “order()” from base R,
+
 #1) rank the scores in “MidtermGradeSheet.csv” by using a function from package “tidyverse”
 #and make sure who got the highest score would receive rank “1” (5pts)
 
@@ -19,5 +20,6 @@ grades <- data.frame(grades,grade_rank) #combines the grades and the student's r
 #3) Save it as a csv file with the name as “ranked_midterm_scores_again” (1.5pt). Tell me where
 #it was saved to (in which folder I can find it). (0.5pt)
 
-write.csv(grades,"ranked_midterm_scores_again.csv",row.names=FALSE)
+write.csv(grades,"ranked_midterm_scores_again.csv",row.names=FALSE) 
+#the file is saved to the working directory, which in my case is /Users/nalahamilton/Desktop/Fall_2023/biostat
 
